ecd221648d3e5b1250e7ec2ab7b5d9aad3bc214c
chalameg <chalamegersa5@gmail.com>
2022-03-30 15:26:30 +0300

    facebok and twitter login

--- passport.js
+++ passport.js
@@ -1,9 +1,12 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport')
+const FacebookStrategy = require('passport-facebook')
 
 
 const GOOGLE_CLIENT_ID = "450824895681-6uar4cut594oj6lfcttojh7l4aqfup7l.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
+const FACEBOOK_APP_ID = "695211021674418"
+const FACEBOOK_APP_SECRET = "b8da02d78e38f940719ff67b420c383b"
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
@@ -16,6 +19,16 @@ passport.use(new GoogleStrategy({
     // });
     cb(null, profile)
   }
+));
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  cb(null, profile)
+}
 ));
 
 passport.serializeUser((user, done) => {
--- routes/auth.js
+++ routes/auth.js
@@ -5,37 +5,58 @@ const passport = require("passport");
 const CLIENT_URL = "http://localhost:3000/";
 
 router.get("/login/success", (req, res) => {
-    if(req.user) {
-      res.status(200).json({
-        success:true,
-        message:"successfull",
-        user: req.user
-      })
-    }
-  });
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+    });
+  }
+});
 
 router.get("/login/failed", (req, res) => {
-    if(req.user) {
-      res.status(401).json({
-        success:false,
-        message:"failure",
-      })
-    }
-  });
-  
-  router.get('/logout', (req, res) => {
-      req.logout();
-      res.redirect(CLIENT_URL)
+  if (req.user) {
+    res.status(401).json({
+      success: false,
+      message: "failure",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(CLIENT_URL);
+});
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/falied",
+  })
+);
+
+router.get("/facebook", passport.authenticate("facebook"));
+
+router.get(
+  "/facebook/callback",
+  passport.authenticate('facebook', { failureRedirect: '/login/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect(CLIENT_URL);
   });
 
-  router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get("/twitter", passport.authenticate("twitter"));
 
-  router.get(
-    "/google/callback",
-    passport.authenticate("google", { 
-      successRedirect: CLIENT_URL,
-      failureRedirect: "/login/falied"
-     })
-  );
+router.get(
+  "/twitter/callback",
+  passport.authenticate('twitter', {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/falied",
+  })
+);
   
-  module.exports = router;
+
+module.exports = router;
